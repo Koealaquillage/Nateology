a5ec55fadb6a267f4f050e1670b80a1a541f1802
Koealaquillage <gkoenig.lamon@gmail.com>
2024-07-04 15:06:47 +0200

    Add a prompt for having the GPT reading the weather

--- Prompts.py
+++ Prompts.py
@@ -13,8 +13,8 @@ class CityCoordinatesGetter():
         self.lat = None
         self.lon = None
         self.template =  PromptTemplate(
-                             input_variables=[self.city_name],
-                             template="What are the latitude and longitude coordinates of {self.city_name}?"
+                             input_variables=[city],
+                             template="What are the latitude and longitude coordinates of {city}?"
                             )
 
        self.llm = OpenAI(engine="gpt-3.5-turbo") 
@@ -22,7 +22,7 @@ class CityCoordinatesGetter():
        self.chain = LLMChain(llm=llm, prompt=template)
 
     def get_city_coordinates(self):
-        response = self.chain.run({"city_name": city_name})
+        response = self.chain.run({"city": self.city_name})
     
         # Using regex to find the coordinates in the response
         match = re.search(r"(-?\d+\.\d+)[^\d,]+(-?\d+\.\d+)", response)
@@ -32,3 +32,21 @@ class CityCoordinatesGetter():
             return [lat, lon]
         else:
             return "Coordinates not found in the response."
+        
+class GetWeatherFromjson():
+
+    def __init__(self, weather_data):
+        self.weather_data = weather_data
+        self.template =  PromptTemplate(
+                             input_variables=[weather],
+                             template="How was the weather described by {weather}? Be very poetic"
+                            )
+
+       self.llm = OpenAI(engine="gpt-3.5-turbo") 
+
+       self.chain = LLMChain(llm=llm, prompt=template)
+
+    def weather_description(self):
+        response = self.chain.run({"weather": self.weather_data})
+    
+        return response[0]
