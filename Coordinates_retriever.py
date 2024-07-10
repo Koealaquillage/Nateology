bc9e39ed6f5ad604319bbb4316474b4ac1d09195
Koealaquillage <gkoenig.lamon@gmail.com>
2024-07-10 11:05:44 +0200

    Changes the coordinates_retriever to use rapid api

--- Coordinates_retriever.py
+++ Coordinates_retriever.py
@@ -1,22 +1,34 @@
-from opencage.geocoder import OpenCageGeocode
-from secret_key import opencagedata_key
+import requests
 
 class CityCoordinatesGetter():
 
-    def __init__(self, city_name):
-        self.city_name = city_name
-        OCG = OpenCageGeocode('opencagedata_key')
+    def __init__(self, apikey):
+        self.base_url = "https://forward-reverse-geocoding.p.rapidapi.com/v1/forward"
+        self.querystring =  {"format":"json","city":"city",
+                             "accept-language":"en","namedetails":"0","limit":"1","bounded":"0",
+                             "polygon_text":"0","polygon_kml":"0","polygon_svg":"0","polygon_geojson":"0",
+                             "polygon_threshold":"0.0"}
+        self.apikey = apikey
+        self.headers = {
+	        "x-rapidapi-key": self.apikey,
+	        "x-rapidapi-host": "forward-reverse-geocoding.p.rapidapi.com"
+            }
+    
+    def _edit_query_string(self, city_name):
+        self.querystring["city"] =  f"{city_name}"
+    
+    def get_city_coordinates(self, city_name):
 
-    def get_city_coordinates(self):
-        results = OCG.geocode(f'{self.city_name}')
-        print(u'%f;%f;%s;%s' % (results[0]['geometry']['lat'],
-                        results[0]['geometry']['lng'],
-                        results[0]['components']['country_code'],
-                        results[0]['annotations']['timezone']['name']))
-        # Using regex to find the coordinates in the response
+        self._edit_query_string(city_name)
+        response = requests.get(self.base_url, headers=self.headers, params=self.querystring)
         try:
-            lat = results[0]['geometry']['lat']
-            lon = results[0]['geometry']['lon']
+            data = response.json()
+            lat = data[0]['lat']
+            lon = data[0]['lon']
             return lat, lon
-        except Exception as e:
-            return {"error": "Could not parse the response correctly.", "details": str(e)}
+        except IndexError:  # Handle empty response data
+            return {"error": "No results found for the specified city."}
+        except KeyError:  # Handle unexpected JSON structure
+            return {"error": "Unexpected data format from API response."}
+        except Exception as e:  # Handle other exceptions such as connection errors
+            return {"error": "Could not retrieve the coordinates.", "details": str(e)}
