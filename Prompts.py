87c1238da7bd0963cb08d1a98ab2483ae4a92907
Koealaquillage <gkoenig.lamon@gmail.com>
2024-07-10 11:10:35 +0200

    Remove the city coordinates getter from the prompts

--- Prompts.py
+++ Prompts.py
@@ -1,36 +1,9 @@
 import re
 from langchain.chains import LLMChain
 from langchain_core.prompts import PromptTemplate
-from langchain_community.llms import OpenAI
+from langchain_openai import OpenAI
 
 
-class CityCoordinatesGetter():
-
-    def __init__(self, city_name):
-        self.city_name = city_name
-
-        self.template =  PromptTemplate(
-                             input_variables=["city"],
-                             template="Please provide the latitude and longitude coordinates of {city} in the format: 'Latitude: <latitude>, Longitude: <longitude>'."
-                            )
-
-        self.llm = OpenAI(temperature=0, max_tokens=30) 
-
-        self.chain = LLMChain(llm=self.llm, prompt=self.template)
-
-    def get_city_coordinates(self):
-        response = self.chain.run({"city": self.city_name})
-        print(response)
-        # Using regex to find the coordinates in the response
-        try:
-            parts = response_text.split(',')
-            lat = float(parts[0].split(':')[1].strip())
-            lon = float(parts[1].split(':')[1].strip())
-            return lat, lon
-        except Exception as e:
-            return {"error": "Could not parse the response correctly.", "details": str(e)}
-
-        
 class GetWeatherFromJson():
     def __init__(self, weather_data):
         self.weather_data = weather_data
